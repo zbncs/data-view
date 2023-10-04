b1383ef876fde0dab9b8642ef77926b1ab9eb5d5
Zhengyi Ma <1204056646@qq.com>
2023-10-04 11:22:35 +0800

    feat：project init, import echarts and element

---
+++ data-report/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <top-view />
+    <sales-view />
+    <map-view />
+    <bottom-view />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import TopView from '@/components/TopView'
+import BottomView from '@/components/BottomView'
+import SalesView from '@/components/SalesView'
+import MapView from '@/components/MapView'
+
+export default {
+  name: 'HomeView',
+  components: {
+    TopView,
+    BottomView,
+    SalesView,
+    MapView
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .home {
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    background: #eee;
+    box-sizing: border-box;
+  }
+</style>
